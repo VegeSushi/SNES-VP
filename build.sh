b13d15cce3d9d0c4cc0baad5cb5f465e89e32f48
VegeSushi <vegesushi19@gmail.com>
2025-07-26 21:58:17 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if ! command -v convert &> /dev/null; then
+    echo "Error: Please install ImageMagick."
+    exit 1
+fi
+
+if ! command -v ffmpeg &> /dev/null; then
+    echo "Error: Please install FFmpeg."
+    exit 1
+fi
+
+if [ -z "$PVSNESLIB_HOME" ]; then
+    echo "Error: PVSnesLib is not detected."
+    exit 1
+fi
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <video_file> <max_frames>"
     exit 1
@@ -17,7 +32,7 @@ MAX_FRAMES="$2"
 GFX_DIR="gfx"
 
 echo Setting up directories...
-mkdir -p gfx
+mkdir -p "$GFX_DIR"
 
 echo Cleaning...
 rm -f "$GFX_DIR"/*.png
@@ -27,7 +42,7 @@ rm -fv linkfile
 make clean
 
 echo Exporting frames...
-ffmpeg -i "$1" -frames:v $2 gfx/frame%03d.png
+ffmpeg -i "$1" -frames:v $2 "$GFX_DIR"/frame%03d.png
 
 echo Running ImageMagick...
 ./magick_all.sh
